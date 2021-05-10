7f1e51e83a9ff2ee1a0086837016a05f3bb788b1
FaizanHaider20083 <faizan20083@iiitd.ac.in>
2021-05-10 13:01:28 +0530

    Add files via upload
    React slick - carousal becomes vertical if dialog is rendered correctly

--- src/Components/ComingSoon/ComingSoonSupplies.js
+++ src/Components/ComingSoon/ComingSoonSupplies.js
@@ -1,13 +1,110 @@
-import React from "react";
+import React,{useState,useEffect} from 'react';
+import PropTypes from 'prop-types';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Typography,
+  CardMedia,
+  Link,
+  Button,
+} from "@material-ui/core";
+import './supply.css';
+import List from '@material-ui/core/List';
+import { blue } from '@material-ui/core/colors';
+import { makeStyles } from '@material-ui/core/styles';
 
-function ComingSoonSupplies(){
-    const description = "Supplies for college students";
-    return(
-        <div className='div'>
-            <h1 className='comingSoon'>Coming Soon</h1>
-            <p className='description'>{description}</p>
-        </div>
-    );
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: blue[100],
+    color: blue[600],
+  },
+});
+const emails = ['username@gmail.com', 'user02@gmail.com'];
+
+function SimpleDialog(props) {
+  const classes = useStyles();
+  const { onClose, selectedValue, open,caption,tips } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+
+  const handleListItemClick = (value) => {
+    onClose(value);
+  };
+
+  return (
+    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+      <DialogTitle id="simple-dialog-title">{caption}</DialogTitle>
+      
+      <List>
+
+      
+
+      
+
+      {tips.map((item,index) => (
+             
+                
+                <img src={item} className="dialogImage" />
+                
+              
+            ))}
+
+            
+
+        
+        </List>
+        
+    </Dialog>
+  );
 }
 
-export default ComingSoonSupplies;
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  selectedValue: PropTypes.string.isRequired,
+};
+
+
+function DisplayCard({Ccard}){
+  const [selectedValue, setSelectedValue] = React.useState(emails[1]);
+  const cardHeight = 250;
+    const cardWidth = cardHeight * (2.5 / 3);
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    
+  return (
+    
+        <div className='container'>
+              <img
+                className="image"
+                src={Ccard.image}
+                alt=""
+                onClick={handleClickOpen}
+                
+                
+                
+              />
+              <p className="Caption" >{Ccard.caption} </p>
+              
+              
+              <SimpleDialog selectedValue={selectedValue} open={open} onClose={handleClose} caption = {Ccard.caption} tips ={Ccard.tips}/>
+              
+      </div>
+
+
+
+  );
+
+}
+
+export default DisplayCard;
--- src/Components/ComingSoon/ComingSoonTools.js
+++ src/Components/ComingSoon/ComingSoonTools.js
@@ -1,16 +1,106 @@
-import React from 'react';
-import './ComingSoonTools.css';
-import Navbar from '../Navbar/Navbar';
+/*Make a card of and exportt it*/ 
+import firebase from 'firebase';
+import React,{useState,useEffect} from 'react';
+import './Rules.css';
+import DisplayCard from './ComingSoonSupplies';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
-function ComingSoonTools() {
-  const description = 'Tools to make your college life easier and productive';
-  return (
-    <div className='div'>
-      <Navbar loggedIn={true} colorStatus={true} />
-      <h1 className='comingSoon'>Coming Soon</h1>
-      <p className='description'>{description}</p>
-    </div>
-  );
+
+
+function ComingSoonTools(){
+    const [data,setData] = useState([]);
+    let temp1 = [];
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 1500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true
+      };
+    
+    
+    
+    
+    const getData = async() => {
+        let cardsData = firebase.database().ref('Tips');
+        await cardsData.once('value',(snapshot) => {
+            snapshot.forEach((childSnapshot) => {
+                let dict = {};
+                
+                dict.image = childSnapshot.val().image;
+                dict.caption = childSnapshot.val().caption;
+                dict.tips = childSnapshot.val().tips;
+                
+                temp1.push(dict);
+                console.log("here");
+            });
+        });
+
+
+        setData(temp1);
+
+    };
+
+
+    useEffect(() => {
+        getData();
+        
+    }, []);
+
+
+    return(
+        
+        <div id="Slider">
+        <br />
+        <h1 className="heading">Rules and regulations </h1>
+        <br />
+        <p className="subHeading">Subheading</p>
+        
+        
+        
+        
+       
+
+        <Slider {...settings}>
+        
+        
+       
+        <ExportCard cardList ={data}></ExportCard>
+        </Slider>
+        
+
+        
+        
+        
+        
+        
+       
+
+        
+        
+       
+        </div>
+       
+        
+    );
+}
+
+function ExportCard({cardList}){
+    
+
+    return cardList.map((card, id) => {
+        
+        return (<div>
+
+        <DisplayCard Ccard ={card}/>
+        
+        </div>)
+        
+      });
 }
 
 export default ComingSoonTools;
+
